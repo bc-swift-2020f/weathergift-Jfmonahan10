c05afad612d261c5029c3ea8df71442f3524368d
James Monahan <54637188+Jfmonahan10@users.noreply.github.com>
2020-10-26 16:48:03 -0400

    Add files via upload
    Weather Gift Final

---
+++ AboutNewViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutNewViewController.swift
+//  WeatherGift
+//
+//  Created by James Monahan on 10/26/20.
+//
+
+import UIKit
+
+class AboutNewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
